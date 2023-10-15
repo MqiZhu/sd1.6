86c0f58814ece3e83e62aa01e55053e94f1b773c
zhumeiqi <zhumeiqi@bigo.sg>
2023-10-15 19:29:33 +0800

    feat: scale

--- modules/task/handler.py
+++ modules/task/handler.py
@@ -4,7 +4,7 @@ from modules.task.zyclients import DrawClient
 #
 from modules.task.oss import upload_to_oss
 from modules.task.log import get_logger
-DrawTaskType = Enum("DrawTaskType", ("txt2img", "img2img", "single"))
+DrawTaskType = Enum("DrawTaskType", ("txt2img", "img2img", "single", "rmbg"))
 DrawTaskStatus = Enum("DrawTaskStatus", ("New", "Fetched",
                       "Drawing", "Succ", "Failed"))
 
@@ -17,6 +17,8 @@ def handle_task(api, client, task_id, task_type, params):
         do_img2img(api, client, task_id, params)
     if task_type == DrawTaskType.single.value:
         do_single(api, client, task_id, params)
+    if task_type == DrawTaskType.rmbg.value:
+        do_rmbg(api, client, task_id, params)
 
 
 def do_txt2img(api, client: DrawClient, task_id, params: dict):
@@ -28,7 +30,7 @@ def do_txt2img(api, client: DrawClient, task_id, params: dict):
     req = StableDiffusionTxt2ImgProcessingAPI()
     params.pop("model_id")
     real_req = req.copy(update=params)
-    images, gen = api.text2imgapi(real_req, from_fetcher=True)
+    images, gen = api.text2img(real_req, from_fetcher=True)
 
     image_info = []
     succ = False
@@ -87,6 +89,34 @@ def do_single(api, client: DrawClient, task_id, params: dict):
     params.pop("model_id")
     real_req = req.copy(update=params)
     images, gen = api.extras_single_image(real_req)
+
+    image_info = []
+    succ = False
+    if len(images) != 0:
+        succ, image_info = upload_to_oss(images)
+
+    status = DrawTaskStatus.Failed
+    if succ:
+        status = DrawTaskStatus.Succ
+
+    client.update_status(task_id,  status, {
+        "images": image_info,
+        "draw_type": "single",
+        "gen_meta": gen
+    })
+
+    logger.info(f"Update status to backend:{image_info}")
+
+
+def do_single(api, client: DrawClient, task_id, params: dict):
+    from modules.api.api import Api
+    from modules.api.models import ExtrasSingleImageRequest
+
+    A: Api = api
+    logger = get_logger()
+    req = ExtrasSingleImageRequest()
+    real_req = req.copy(update=params)
+    images, gen = api.r(real_req)
 
     image_info = []
     succ = False
