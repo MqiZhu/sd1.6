f63ec1185f8e0608e2498fe8d6a17aeb91b90db9
YashonAlbert <yashonalbert@YashonAlbertdeMacBook-Pro.local>
2024-04-01 15:02:19 +0800

    feat: update upload_to_oss

--- modules/task/oss.py
+++ modules/task/oss.py
@@ -34,25 +34,22 @@ def upload_to_oss(images_gen) -> bool:
     images = []
 
     for image in images_gen:
-        id = gen_aigc_oss_id()
-        oss_name = "{}.png".format(id)
+        oss_name = f"{gen_aigc_oss_id()}.png"
 
-        buf = BytesIO()
-        image.save(buf, 'png')
-
-        img_data = buf.getvalue()
-
-
-        result = bucket.put_object(oss_name, img_data)
-        buf.close()
-
-        images.append({
-            "url": "https://cdn.magicwand.so/{}".format(oss_name),
-            "bucket": "zy-pic-items-test",
-            "size": len(img_data)
-        })
-
-    return True, images
+        with BytesIO() as buf:
+            image.save(buf, 'png')
+            img_data = buf.getvalue()
+            bucket_name = "magic-wand-sd"
+            upload_to_google_oss(bucket_name, img_data, oss_name)
+            images.append({
+                "url": f"https://storage.googleapis.com/magic-wand-sd/{oss_name}",
+                "bucket": bucket_name,
+                "size": len(img_data)
+            })
+        # result = bucket.put_object(oss_name, img_data)
+        # buf.close()
+            
+    return len(images_gen) == len(images), images
 
 
 def get_image_from_oss(task_id, path, bucket_name=default_bucket_name):
@@ -75,3 +72,26 @@ def upload_to_puzzle(pic_id, image):
     bucket_puzzle.put_object(key, image)
     return f"https://cdn-puzzle.magicwand.so/tmp/{pic_id}.png"
 
+
+from google.cloud import storage
+from google.oauth2 import service_account
+
+def upload_to_google_oss(bucket_name, source_file, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # The path to your service account key file
+    key_path = "/home/puncsky/service-account-gcs.json"
+
+    # Explicitly use service account credentials by specifying the private key file.
+    credentials = service_account.Credentials.from_service_account_file(key_path)
+
+    # Initialize the Google Cloud Storage client with your credentials
+    storage_client = storage.Client(credentials=credentials, project=credentials.project_id)
+
+    # Get the bucket
+    bucket = storage_client.bucket(bucket_name)
+
+    # Create a new blob (file) in the bucket and upload the file's content
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_string(source_file)
+
+    print(f"Google Cloud Storage: file uploaded to {destination_blob_name}.")
